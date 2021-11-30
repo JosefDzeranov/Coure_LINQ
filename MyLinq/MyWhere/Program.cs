6cacb322e53080ba60693d7dbda2d8aed823d5ce
Josef Dzeranov <iodzeranov@mail.ru>
2021-11-30 14:07:51 +0300

    MyWhere

--- MyLinq/MyWhere/Program.cs
+++ MyLinq/MyWhere/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyWhere
 {
-    class Program
+    static class Program
     {
+        static IEnumerable<T> MyWhere<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+        {
+            var result = new List<T>();
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var source = new int[5] {1, 2, 3, 4, 5};
+            var result = source.Where(x => x > 2);
+            Console.WriteLine(string.Join(", ", result));
         }
     }
 }
